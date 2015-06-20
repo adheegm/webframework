7a1d42bfab985bb125d4a7ea42bd7740f3f794c5
Adi <adheegm@yahoo.com>
2015-06-20 14:28:40 +0700

    update

--- php/views/html/head/index.php
+++ php/views/html/head/index.php
@@ -2,6 +2,7 @@
 	<base href="<?php print $__SITE__HOME__ADDRESS__; ?>/">
 	<meta name="keywords" content="adi winata, technology, programming, .net, c#, html, css, javascript, jquery, mobile, game development, webgl, 3d, php, apache, android, java, open source, music, blues, black blues, texas blues, youtube, facebook, twitter, books, indonesia, sumatera, kalimantan, jawa, sulawesi, papua, bike, mountain">
 	<meta name="description" content="Adi Winata's personal website for everythings about what i don't known and what i known. Just share for people who want to learn about everythings. Programming, science, music and live.">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="author" content="Adi Winata">
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8">
 	<title><?php print $__SITE__TITLE__; ?></title>
